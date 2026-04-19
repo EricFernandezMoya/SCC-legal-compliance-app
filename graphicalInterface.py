fd20c2d86e42fd7d9347ee7cbf073ade2bdb07e3
Eric Fernandez Moya <nando_eric_85@hotmail.com>
2026-04-19 22:48:45 +1000

    new code

--- graphicalInterface.py
+++ graphicalInterface.py
@@ -1,7 +1,7 @@
-from anthropicConnection import textQueryToAntropic
+from claudeConnection import textQueryToAntropic
 import tkinter as tk
-from tkinter import Frame, Label, Button, Toplevel, Text
-
+from tkinter import Frame, Label, Button, Toplevel, Text, PhotoImage, filedialog
+from tkcalendar import DateEntry
 
 def show_frame(frame):
     frame.tkraise()
@@ -25,13 +25,15 @@ show_frame(mainFrame)
 label = Label(mainFrame, text="Home Page", font=("Arial", 16))
 label.pack(pady=20)
         
-buttonSettingPage = Button(mainFrame, text="Go to Settings", command=lambda: show_frame(anthropicFrame))
+buttonSettingPage = Button(mainFrame, text="Go to AI Query", command=lambda: show_frame(anthropicFrame))
 buttonSettingPage.pack(pady=20)
 
         
 buttonInsertLegalDocument = Button(mainFrame, text="Insert Doc", command= lambda: insertDocWindows())
 buttonInsertLegalDocument.pack(pady=20)
 
+buttonUpdateLegalDocument = Button(mainFrame, text="Update Doc", command= lambda: updateDocWindows())
+buttonUpdateLegalDocument.pack(pady=20)
 
 
 
@@ -66,7 +68,8 @@ def insertDocWindows():
     new_window.geometry("500x500")
 
     
-    Label(new_window, text="Insert Legal Document", font=("Arial", 16)).place(x=10, y=10)
+    titleLabel = Label(new_window, text="Insert Legal Document", font=("Arial", 16))
+    titleLabel.place(x=10, y=10)
 
     Label(new_window, text="Document name:").place(x=10, y=100)
     Text(new_window, width=50, height=1).place(x=10, y=120)
@@ -76,6 +79,62 @@ def insertDocWindows():
 
     Label(new_window, text="Description:").place(x=10, y=200)
     Text(new_window, width=50, height=15).place(x=10, y=220)
+
+def selectFilePath(filePathText, parent_window):
+    types = [("Data Files", "*.pdf *.doc *.docx *.odt"), ("All Files", "*.*")]
+    filePath = filedialog.askopenfilename(title="Select a File", filetypes=types)
+    
+    parent_window.deiconify()
+    parent_window.lift()
+    parent_window.focus_force()
+    
+    if filePath:
+        filePathText.delete("1.0", "end")
+        filePathText.insert("1.0", filePath)
+
+def updateDocWindows():
+    
+    new_window = Toplevel(root)
+    new_window.title("Insert Legal Document")
+    new_window.geometry("500x500")
+    new_window.transient(root)
+    new_window.grab_set()
+    new_window.focus_force()
+
+    versionLabel = Label(new_window, text="Version:")
+    versionLabel.place(x=10, y=100)
+    versionText = Text(new_window, width=50, height=1)
+    versionText.place(x=10, y=120)
+
+    effectiveFromLabel = Label(new_window, text="Effective from:")
+    effectiveFromLabel.place(x=10, y=150)
+    effectiveFromDateEntry = DateEntry(new_window, width=10, background='darkblue', foreground='white', borderwidth=2)
+    effectiveFromDateEntry.place(x=10, y=170)
+
+    effectiveToLabel = Label(new_window, text="Effective to:")
+    effectiveToLabel.place(x=150, y=150)
+    effectiveToDateEntry = DateEntry(new_window, width=10, background='darkblue', foreground='white', borderwidth=2)
+    effectiveToDateEntry.place(x=150, y=170)
+
+    sourceUrlLabel = Label(new_window, text="Source url:")
+    sourceUrlLabel.place(x=10, y=200)
+    sourceUrlText = Text(new_window, width=50, height=1)
+    sourceUrlText.place(x=10, y=220)
+
+    filePathLabel = Label(new_window, text="File path:")
+    filePathLabel.place(x=10, y=250)
+    filePathText = Text(new_window, width=50, height=1)
+    filePathText.place(x=10, y=270)
+    filePathIcon = PhotoImage(file="searchFileIcon.png").subsample(8, 8)
+
+    filePathButton = Button(new_window, image=filePathIcon, command=lambda: selectFilePath(filePathText, new_window), padx=5, pady=5)
+    filePathButton.image = filePathIcon  # prevent garbage collection
+    filePathButton.place(x=430, y=270)
+    
+    notesLabel = Label(new_window, text="Description:")
+    notesLabel.place(x=10, y=300)
+    notesText = Text(new_window, width=50, height=5)
+    notesText.place(x=10, y=320)
 
 
 root.mainloop()
